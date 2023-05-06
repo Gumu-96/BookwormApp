01f504b3b5b0c65347e3f67260b4cdbc6bd13868
Gustavo Müller <muller.gustavo@outlook.com>
2023-05-06 12:29:52 -0600

    :construction: Initial hilt setup

--- app/src/main/java/com/gumu/bookwormapp/MainActivity.kt
+++ app/src/main/java/com/gumu/bookwormapp/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.gumu.bookwormapp.presentation.theme.BookwormAppTheme
 import com.gumu.bookwormapp.presentation.ui.signin.SignInScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
