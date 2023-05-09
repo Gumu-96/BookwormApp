d5bf07c4960893590436e13e79fc6f82ff8da7e3
Gustavo Müller <muller.gustavo@outlook.com>
2023-05-09 12:17:09 -0600

    :bug: Fix di issue

--- app/src/main/java/com/gumu/bookwormapp/data/util/DispatchersProviderImpl.kt
+++ app/src/main/java/com/gumu/bookwormapp/data/util/DispatchersProviderImpl.kt
@@ -2,8 +2,9 @@ package com.gumu.bookwormapp.data.util
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
 
-class DispatchersProviderImpl : DispatchersProvider {
+class DispatchersProviderImpl @Inject constructor() : DispatchersProvider {
     override val main: CoroutineDispatcher
         get() = Dispatchers.Main
     override val io: CoroutineDispatcher
