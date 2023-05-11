fe9da9c2a6ef0612843dd60ab4022a745fa33d20
Gustavo Müller <muller.gustavo@outlook.com>
2023-05-11 11:29:17 -0600

    :fire: Remove unused val

--- app/src/main/java/com/gumu/bookwormapp/presentation/ui/search/SearchState.kt
+++ app/src/main/java/com/gumu/bookwormapp/presentation/ui/search/SearchState.kt
@@ -8,7 +8,6 @@ import com.gumu.bookwormapp.domain.model.Book
 import kotlinx.coroutines.flow.Flow
 
 data class SearchState(
-    val isLoading: Boolean = false,
     val searchQuery: String = "",
     val books: Flow<PagingData<Book>>? = null,
     val filterOptions: SearchFilterOptions = SearchFilterOptions()
