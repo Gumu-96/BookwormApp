e21eb9dabd4babc961bd4ef895376c29705d8a87
Gustavo Müller <muller.gustavo@outlook.com>
2023-05-18 09:02:54 -0600

    :lipstick: Add vertical scroll to stats screen

--- app/src/main/java/com/gumu/bookwormapp/presentation/ui/bookstats/BookStatsScreen.kt
+++ app/src/main/java/com/gumu/bookwormapp/presentation/ui/bookstats/BookStatsScreen.kt
@@ -12,8 +12,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DeleteForever
 import androidx.compose.material.icons.filled.Done
@@ -140,7 +142,7 @@ fun StatsContent(
         modifier = Modifier
             .fillMaxSize()
             .then(modifier)
-        //.verticalScroll(rememberScrollState())
+            .verticalScroll(rememberScrollState())
     ) {
         BookSection(book = book)
         Divider(
