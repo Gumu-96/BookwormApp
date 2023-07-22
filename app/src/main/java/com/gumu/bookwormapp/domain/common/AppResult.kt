08ebe9703f8f2c9477af442d17dd355f0d7ca7d7
Gustavo Müller <muller.gustavo@outlook.com>
2023-07-22 16:08:24 -0600

    :fire: Remove unused NoInternet result

--- app/src/main/java/com/gumu/bookwormapp/domain/common/AppResult.kt
+++ app/src/main/java/com/gumu/bookwormapp/domain/common/AppResult.kt
@@ -1,28 +1,22 @@
 package com.gumu.bookwormapp.domain.common
 
-sealed class AppResult<out T: Any?> {
-    data class Success<out T: Any?> (val data: T) : AppResult<T>()
+sealed class AppResult<out T : Any?> {
+    data class Success<out T : Any?>(val data: T) : AppResult<T>()
     data class Failure(val error: AppError) : AppResult<Nothing>()
-    object Loading: AppResult<Nothing>()
-    object NoInternet: AppResult<Nothing>()
+    object Loading : AppResult<Nothing>()
 }
 
-inline fun <T: Any?> AppResult<T>.onSuccess(action: (T) -> Unit): AppResult<T> {
+inline fun <T : Any?> AppResult<T>.onSuccess(action: (T) -> Unit): AppResult<T> {
     if (this is AppResult.Success) action(data)
     return this
 }
 
-inline fun <T: Any?> AppResult<T>.onFailure(action: (AppError) -> Unit): AppResult<T> {
+inline fun <T : Any?> AppResult<T>.onFailure(action: (AppError) -> Unit): AppResult<T> {
     if (this is AppResult.Failure) action(error)
     return this
 }
 
-inline fun <T: Any?> AppResult<T>.onLoading(action: () -> Unit): AppResult<T> {
+inline fun <T : Any?> AppResult<T>.onLoading(action: () -> Unit): AppResult<T> {
     if (this is AppResult.Loading) action()
-    return this
-}
-
-inline fun <T: Any?> AppResult<T>.onNoInternet(action: () -> Unit): AppResult<T> {
-    if (this is AppResult.NoInternet) action()
     return this
 }
