baa87fd7f0a2b15863be46815b3bcbfd2dc3a59e
Gustavo Müller <gumu96.dev@gmail.com>
2023-11-05 09:50:02 -0600

    :recycle: Improve VM events collection

--- app/src/main/java/com/gumu/bookwormapp/presentation/ui/common/ScreenWrapper.kt
+++ app/src/main/java/com/gumu/bookwormapp/presentation/ui/common/ScreenWrapper.kt
@@ -4,7 +4,13 @@ import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 
 @Composable
 fun <State, ScreenEvent> ScreenWrapper(
@@ -14,21 +20,31 @@ fun <State, ScreenEvent> ScreenWrapper(
 ) {
     val context = LocalContext.current
 
-    LaunchedEffect(key1 = Unit) {
-        viewModel.uiEvents.collect { event ->
-            when (event) {
-                is UiEvent.ShowToast -> {
-                    Toast.makeText(context, event.message, Toast.LENGTH_LONG).show()
-                }
-                is UiEvent.NavigateBack -> navController.popBackStack()
-                is UiEvent.NavigateTo -> navController.navigate(event.route) {
-                    event.popUpTo?.let {
-                        popUpTo(it) { inclusive = true }
-                    }
+    ObserveAsEvents(viewModel.uiEvents) { event ->
+        when (event) {
+            is UiEvent.ShowToast -> {
+                Toast.makeText(context, event.message, Toast.LENGTH_LONG).show()
+            }
+            is UiEvent.NavigateBack -> navController.popBackStack()
+            is UiEvent.NavigateTo -> navController.navigate(event.route) {
+                event.popUpTo?.let {
+                    popUpTo(it) { inclusive = true }
                 }
             }
         }
     }
 
     content()
+}
+
+@Composable
+private fun <T> ObserveAsEvents(flow: Flow<T>, onEvent: (T) -> Unit) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    LaunchedEffect(flow, lifecycleOwner.lifecycle) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            withContext(Dispatchers.Main.immediate) {
+                flow.collect(onEvent)
+            }
+        }
+    }
 }
