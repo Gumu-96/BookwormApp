afdaed77cd7e33e4be5f984eee90a40e447b84e1
Gustavo Müller <gumu96.dev@gmail.com>
2025-04-25 11:09:52 -0600

    💄 Enable edge to edge on sdk 34 and lower

--- app/src/main/java/com/gumu/bookwormapp/MainActivity.kt
+++ app/src/main/java/com/gumu/bookwormapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gumu.bookwormapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.gumu.bookwormapp.presentation.navigation.BookwormNavigation
 import com.gumu.bookwormapp.presentation.theme.BookwormAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,6 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             BookwormAppTheme {
