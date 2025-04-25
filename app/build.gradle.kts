1c6f275b650de478904d1c99bd43b09776fcd8bf
Gustavo Müller <gumu96.dev@gmail.com>
2025-04-25 10:13:12 -0600

    🔧 Bump target/compile sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
     namespace = "com.gumu.bookwormapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.gumu.bookwormapp"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
