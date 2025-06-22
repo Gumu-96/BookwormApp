eb96663f9ba49127e73784598191fb3d20103fea
Gustavo Müller <gumu96.dev@gmail.com>
2025-06-22 11:58:04 -0600

    ♻️ Simplify connectivity observer

--- app/src/main/java/com/gumu/bookwormapp/presentation/util/ConnectivityObserver.kt
+++ app/src/main/java/com/gumu/bookwormapp/presentation/util/ConnectivityObserver.kt
@@ -3,9 +3,8 @@ package com.gumu.bookwormapp.presentation.util
 import kotlinx.coroutines.flow.Flow
 
 interface ConnectivityObserver {
-    fun observeConnectivity(): Flow<ConnectivityStatus>
 
-    enum class ConnectivityStatus {
-        AVAILABLE, UNAVAILABLE, LOOSING, LOST
-    }
+    fun isConnected(): Boolean
+
+    fun observe(): Flow<Boolean>
 }
--- app/src/main/java/com/gumu/bookwormapp/presentation/util/NetworkConnectivityObserver.kt
+++ app/src/main/java/com/gumu/bookwormapp/presentation/util/NetworkConnectivityObserver.kt
@@ -3,41 +3,55 @@ package com.gumu.bookwormapp.presentation.util
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
-import com.gumu.bookwormapp.presentation.util.ConnectivityObserver.ConnectivityStatus
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
-import javax.inject.Inject
 
-class NetworkConnectivityObserver @Inject constructor(
+class NetworkConnectivityObserver(
     context: Context
 ) : ConnectivityObserver {
     private val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-    override fun observeConnectivity(): Flow<ConnectivityStatus> = callbackFlow {
-        val callback = object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                super.onAvailable(network)
-                trySend(ConnectivityStatus.AVAILABLE)
-            }
+    private fun NetworkCapabilities.internetCapabilityValidated(): Boolean {
+        return hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+    }
 
-            override fun onLosing(network: Network, maxMsToLive: Int) {
-                super.onLosing(network, maxMsToLive)
-                trySend(ConnectivityStatus.LOOSING)
-            }
+    override fun isConnected(): Boolean {
+        val network = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return capabilities.internetCapabilityValidated()
+    }
+
+    override fun observe(): Flow<Boolean> = callbackFlow {
+        val callback = object : ConnectivityManager.NetworkCallback() {
 
             override fun onLost(network: Network) {
                 super.onLost(network)
-                trySend(ConnectivityStatus.LOST)
+                trySend(false)
             }
 
             override fun onUnavailable() {
                 super.onUnavailable()
-                trySend(ConnectivityStatus.UNAVAILABLE)
+                trySend(false)
+            }
+
+            override fun onCapabilitiesChanged(
+                network: Network,
+                networkCapabilities: NetworkCapabilities
+            ) {
+                trySend(networkCapabilities.internetCapabilityValidated())
             }
         }
-        connectivityManager.registerDefaultNetworkCallback(callback)
+        val request = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+            .build()
+
+        connectivityManager.registerNetworkCallback(request, callback)
         awaitClose { connectivityManager.unregisterNetworkCallback(callback) }
     }.distinctUntilChanged()
 }
